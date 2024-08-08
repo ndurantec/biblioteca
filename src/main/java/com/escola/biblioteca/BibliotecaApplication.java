e82897fd61c75369635bb0b42b234e08892f2d08
JavaliRobusto <vitor.bessa@escola.pr.gov.br>
2024-08-08 11:44:32 -0300

    Arquivos Iniciais

---
+++ src/main/java/com/escola/biblioteca/BibliotecaApplication.java
@@ -0,0 +1,15 @@
+package com.escola.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class BibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApplication.class, args);
+	}
+
+}
