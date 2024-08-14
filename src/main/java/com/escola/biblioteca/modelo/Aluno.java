6309edbb506415d0807748c003ffa5f4399a7f7b
JavaliRobusto <vitor.bessa@escola.pr.gov.br>
2024-08-14 12:21:57 -0300

    criei a classe aluno

---
+++ src/main/java/com/escola/biblioteca/modelo/Aluno.java
@@ -0,0 +1,48 @@
+package com.escola.biblioteca.modelo;
+
+public class Aluno {
+
+    String nome;
+    String endereco;
+    String email;
+    int cgm;
+    String telefone;
+    //date dataNascimento;
+
+
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getCgm() {
+        return cgm;
+    }
+    public void setCgm(int cgm) {
+        this.cgm = cgm;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+
+   
+    
+}
