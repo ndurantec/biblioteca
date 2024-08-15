370b6afd47b0f97eef1d9c9b21a068b45435a973
JavaliRobusto <vitor.bessa@escola.pr.gov.br>
2024-08-15 11:42:34 -0300

    classe aluno e emprestimo

--- src/main/java/com/escola/biblioteca/modelo/Aluno.java
+++ src/main/java/com/escola/biblioteca/modelo/Aluno.java
@@ -2,15 +2,21 @@ package com.escola.biblioteca.modelo;
 
 public class Aluno {
 
-    String nome;
-    String endereco;
-    String email;
-    int cgm;
-    String telefone;
-    //date dataNascimento;
+    private  String nome;
+    private String endereco;
+    private String email;
+    private int cgm;
+    private String telefone;
+    private String dataNascimento;
 
 
     
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
     public String getNome() {
         return nome;
     }
@@ -41,6 +47,58 @@ public class Aluno {
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
+    public Aluno(String nome, String endereco, String email, int cgm, String telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.email = email;
+        this.cgm = cgm;
+        this.telefone = telefone;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + cgm;
+        result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Aluno other = (Aluno) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (endereco == null) {
+            if (other.endereco != null)
+                return false;
+        } else if (!endereco.equals(other.endereco))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (cgm != other.cgm)
+            return false;
+        if (telefone == null) {
+            if (other.telefone != null)
+                return false;
+        } else if (!telefone.equals(other.telefone))
+            return false;
+        return true;
+    }
+    
 
 
    
