4fc2de3601875f5c01690d735d1d08c9d649c334
nanojha12 <jose.altissimo@escola.pr.gov.br>
2024-08-22 12:03:23 -0300

    classe emprestimo criada

--- src/main/java/com/escola/biblioteca/controller/EmprestimoController.java
+++ src/main/java/com/escola/biblioteca/controller/EmprestimoController.java
@@ -1,8 +1,22 @@
 package com.escola.biblioteca.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/emprestimo")
 public class EmprestimoController {
-    
-}
+
+    @GetMapping(value = "/imprimir")
+    public String imprimir(){
+        System.out.println("cheugouu");
+        return "okok";
+    }
+
+    @GetMapping(value = "/criar")
+    public String criar () {
+        System.out.println("cheugouu");
+        return "okok";  
+     }
+ }
