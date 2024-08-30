a0151dee5aa6010dacd4cce8d19dfd9177f296cd
BARELLA261121 <erike.barella@escola.pr.gov.br>
2024-08-30 09:22:16 -0300

    AlunoController

--- src/main/java/com/escola/biblioteca/modelo/Aluno.java
+++ src/main/java/com/escola/biblioteca/modelo/Aluno.java
@@ -11,8 +11,6 @@ public class Aluno {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-
-  
     private  String nome;
     private String endereco;
     private String email;
@@ -20,62 +18,90 @@ public class Aluno {
     private String telefone;
     private String dataNascimento;
 
+    @Deprecated
+    public Aluno() {
+    }
 
+    public Aluno(Long id, String nome, String endereco, String email, int cgm, String telefone, String dataNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.email = email;
+        this.cgm = cgm;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getCgm() {
+        return cgm;
+    }
+
+    public void setCgm(int cgm) {
+        this.cgm = cgm;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 
     public String getDataNascimento() {
         return dataNascimento;
     }
+
     public void setDataNascimento(String dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getEndereco() {
-        return endereco;
-    }
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public int getCgm() {
-        return cgm;
-    }
-    public void setCgm(int cgm) {
-        this.cgm = cgm;
-    }
-    public String getTelefone() {
-        return telefone;
-    }
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-    public Aluno(String nome, String endereco, String email, int cgm, String telefone) {
-        this.nome = nome;
-        this.endereco = endereco;
-        this.email = email;
-        this.cgm = cgm;
-        this.telefone = telefone;
-    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
         result = prime * result + ((nome == null) ? 0 : nome.hashCode());
         result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
         result = prime * result + ((email == null) ? 0 : email.hashCode());
         result = prime * result + cgm;
         result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+        result = prime * result + ((dataNascimento == null) ? 0 : dataNascimento.hashCode());
         return result;
     }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -85,6 +111,11 @@ public class Aluno {
         if (getClass() != obj.getClass())
             return false;
         Aluno other = (Aluno) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
         if (nome == null) {
             if (other.nome != null)
                 return false;
@@ -107,7 +138,17 @@ public class Aluno {
                 return false;
         } else if (!telefone.equals(other.telefone))
             return false;
+        if (dataNascimento == null) {
+            if (other.dataNascimento != null)
+                return false;
+        } else if (!dataNascimento.equals(other.dataNascimento))
+            return false;
         return true;
     }
- 
+
+    @Override
+    public String toString() {
+        return "Aluno [id=" + id + ", nome=" + nome + ", endereco=" + endereco + ", email=" + email + ", cgm=" + cgm
+                + ", telefone=" + telefone + ", dataNascimento=" + dataNascimento + "]";
+    }
 }
