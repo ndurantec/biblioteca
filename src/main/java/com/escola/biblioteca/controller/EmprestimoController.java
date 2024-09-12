079279d49f133f61b6750a47869488fed93698ec
gurgel15 <gabriel.gurgel@escola.pr.gov.br>
2024-09-12 10:52:41 -0300

    atualizacao na classe emprestio controller

--- src/main/java/com/escola/biblioteca/controller/EmprestimoController.java
+++ src/main/java/com/escola/biblioteca/controller/EmprestimoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,6 +66,7 @@ public class EmprestimoController {
                     .orElse(ResponseEntity.notFound().build());
     }
 
+    @DeleteMapping (value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         emprestimoRepository.deleteById(id);
             
