d11efa7d5c951274848494976e3e0906ba2d13a1
gurgel15 <gabriel.gurgel@escola.pr.gov.br>
2024-09-18 12:17:09 -0300

    arrumnaod tudo que tava errado

--- src/main/java/com/escola/biblioteca/controller/EmprestimoController.java
+++ src/main/java/com/escola/biblioteca/controller/EmprestimoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.escola.biblioteca.modelo.Emprestimo;
 import com.escola.biblioteca.repository.EmprestimoRepository;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping(value = "/emprestimo")
 public class EmprestimoController {
 
