dd6dba635f13c9dd627fb5dff95112c2d100da6d
gurgel15 <gabriel.gurgel@escola.pr.gov.br>
2024-09-19 11:43:59 -0300

    ggggg

--- src/main/java/com/escola/biblioteca/controller/AlunoController.java
+++ src/main/java/com/escola/biblioteca/controller/AlunoController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +21,8 @@ import com.escola.biblioteca.Dto.AlunoDto;
 import com.escola.biblioteca.modelo.Aluno;
 import com.escola.biblioteca.repository.AlunoRepository;
 
- @RestController  
+ @RestController
+ @CrossOrigin("*")
  @RequestMapping(value = "/aluno")
     public class AlunoController {
 
